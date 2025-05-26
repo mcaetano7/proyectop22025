c7489510fba5e4d21f4712593ab58bc2031cb44f
VictoriaMoraless <victoria.moraless@correo.ucu.edu.uy>
2025-05-26 20:54:03 -0300

    almacen

--- src/Library/Almacen.cs
+++ src/Library/Almacen.cs
@@ -1,6 +1,19 @@
 namespace Library;
 
-public class Almacen
+public class Almacen : Edificio
 {
-    
+    public string Tipo { get; set; }
+    public int Capacidad { get; set; }
+
+    public Almacen(Coordenada ubicacion, int vida, Player owner, string tipo, int capacidad)
+        : base(ubicacion, vida, owner)
+    {
+        Tipo = tipo;
+        Capacidad = capacidad;
+    }
+
+    public override string ToString()
+    {
+        return $"Almacen de {Tipo} (Capacidad: {Capacidad}) en {Ubicacion}, Vida: {Vida}, Dueño: {Owner}";
+    }
 }
