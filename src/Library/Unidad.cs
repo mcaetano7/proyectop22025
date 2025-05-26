efe0e862d8e5d7995cb81757baf6feffb08d3399
VictoriaMoraless <victoria.moraless@correo.ucu.edu.uy>
2025-05-26 20:39:57 -0300

    Unidad

--- src/Library/Unidad.cs
+++ src/Library/Unidad.cs
@@ -1,6 +1,21 @@
 namespace Library;
 
-public class Unidad
+public abstract class Unidad
 {
-    
+    public int Id { get; set; }
+    public Coordenada Ubicacion { get; set; }
+    public int Vida { get; set; }
+    public int Velocidad { get; set; }
+    public Player Owner { get; set; }
+
+    public Unidad(int id, Coordenada ubicacion, int vida, int velocidad, Player owner)
+    {
+        Id = id;
+        Ubicacion = ubicacion;
+        Vida = vida;
+        Velocidad = velocidad;
+        Owner = owner;
+    }
+
+    public abstract void Mover(Coordenada nuevaUbicacion);
 }
