9ab372d1da21b689eb69a95751b5cc577e7775bd
VictoriaMoraless <victoria.moraless@correo.ucu.edu.uy>
2025-05-26 19:06:02 -0300

    Civilizacion

--- src/Library/civilizacion.cs
+++ src/Library/civilizacion.cs
@@ -2,7 +2,16 @@ namespace Library
 {
     public class Civilizacion
     {
-    
+        public string Name { get; set; }
+        public List<string> Bonificaciones { get; set; } 
+        public TipoUnidad UnidadEspecial { get; set; }
+
+        public Civilizacion(string name, List<string> Bonificaciones, TipoUnidad unidadEspecial)
+        {
+        Name = name;
+        Bonificaciones = Bonificaciones;
+        UnidadEspecial = unidadEspecial;
+        }
     }
     
 }
