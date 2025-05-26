2575cbca03b7077bd73702ba9dd9b6079ef764e8
Mik4desuu <mikaela.maldonado@correo.ucu.edu.uy>
2025-05-26 18:56:22 -0300

    agrego historias de usuario

---
+++ src/Library/Facade.cs
@@ -0,0 +1,26 @@
+namespace Library;
+
+
+/* HISTORIAS DE USUARIO:
+ 
+ 1. poder crear una nueva partida especificando el tamaño del mapa y cantidad de jugadores.
+ 2. elegir civilización (aprovechar sus ventajas estratégicas)
+ 3. comenzar con un centro cívico y algunos aldeanos para iniciar la recolecci{on de recursos.
+ 4. ordenar a los aldeanos recolectar diferentes tipos de recursos.
+ 5. construir edificios para almacenar recursos.
+ 6. quiero visualizar la cantidad de recursos disponibles.
+ 7. construir edificios en ubis específicas para expandir la base.
+ 8. crear diferentes tipos de edificios con funciones específicas para desarrollar la civilización.
+ 9. entrrenar unidades militares para defender la base y atacar oponentes.
+ 10. mover las unidades ppor el mapa usando comandos simples.
+ 11. ordenar a las unidades atacar edeficios o unidades enemigas. 
+ 12. entrenar aldeanos parta mejorar la econom{ia y tener suficientes casas para mantener la población.
+ 13. destruir los centros cívicos para ganar la partida por dominación militar.
+ 14. usar comandos intuitivos para interactuar con el juego 
+ 15. quiero ver un mapa simplificado del juego en ASCII para visualizar la dispo del terreno y unidades. 
+ */
+
+public class Facade
+{
+    
+}
