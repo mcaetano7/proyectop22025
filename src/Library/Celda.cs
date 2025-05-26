5d192d4224875a1e17b040c7bcf51ad5fa1a7a56
beluume <maria.mendes@correo.ucu.edu.uy>
2025-05-26 19:04:11 -0300

    clase celda

---
+++ src/Library/Celda.cs
@@ -0,0 +1,24 @@
+﻿namespace Library;
+
+public class Celda
+{
+    private Coordenada coordenada;
+    private List<ElementoMapa> contenido;
+    
+    public Celda(Coordenada coordenada)
+    {
+        this.coordenada = coordenada;
+        this.contenido = new List<ElementoMapa>();
+    }
+    
+    public Coordenada Coordenada
+    {
+        get { return coordenada; }
+        set { coordenada = value; }
+    }
+
+    public List<ElementoMapa> Contenido
+    {
+        get { return contenido; }
+    }
+}
