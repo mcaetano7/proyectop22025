bed76a093fac24ec5e076182705fc42fb903e559
Mik4desuu <mikaela.maldonado@correo.ucu.edu.uy>
2025-05-28 16:27:50 -0300

    relleno caballeria

--- src/Library/Caballeria.cs
+++ src/Library/Caballeria.cs
@@ -1,6 +1,10 @@
 namespace Library;
 
-public class Caballeria
+public class Caballeria : UnidadMilitar
 {
-    
+    public Caballeria(int id, Coordenada ubicacion, Player owner)
+        : base(id, ubicacion, vida: 100, velocidad: 90, owner, ataque: 40)
+    {
+        Defensa = 20;  // Definís valores propios para cada tipo
+    }  
 }
