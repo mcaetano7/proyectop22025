798fae1be54fcde64067f925f64ab0b2e74458d9
Mik4desuu <mikaela.maldonado@correo.ucu.edu.uy>
2025-05-28 16:19:44 -0300

    agrego unidad militar (flata definir métodos)

--- src/Library/UnidadMilitar.cs
+++ src/Library/UnidadMilitar.cs
@@ -1,8 +1,33 @@
 namespace Library;
 
-public class UnidadMilitar
+public class UnidadMilitar : Unidad
 {
-    //se guardan en el cuartel pero necesitamos hacer casas
-    //si tenes 20 aldeanos y 20 militares necesitas 8 casas
-    //porque tambien cuenta que los militares precisan una casa
+   public int Ataque { get; set; }
+   
+   public UnidadMilitar(int id, Coordenada ubicacion, int vida, int velocidad, Player owner, int ataque)
+      : base(id, ubicacion, vida, velocidad, owner)
+          
+   {
+       Ataque = ataque; 
+   }
+   
+   public void Mover(Coordenada nuevaUbicacion)
+   {
+       //implementar codigo if mapa tiene posicion válida blah blah 
+       //para eso crear clase mapa para ver los métodos 
+   }
+   
+   public void Atacar(Unidad objetivo)
+   {
+       // idem lo de arriba, ver como puede atacar la unidad militar 
+   }
+   
+   public int Defensa { get; set; } 
+
+   public void Defender()
+   {
+       //l{ogica para ver el daño que recibio y actualizar la vida d la unidad
+   }
+   
+   
 }
