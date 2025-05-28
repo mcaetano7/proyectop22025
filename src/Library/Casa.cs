cdc2c12ee6439709efb21db097b1644b69b65094
Mik4desuu <mikaela.maldonado@correo.ucu.edu.uy>
2025-05-28 20:34:04 -0300

    clase casa

--- src/Library/Casa.cs
+++ src/Library/Casa.cs
@@ -1,6 +1,15 @@
 namespace Library;
 
-public class Casa
+public class Casa : Edificio
 {
-    
+  //capacidad poblacion
+  public int CapacidadPoblacion { get; set; }
+  public int CapacidadMaxima = 5;
+  
+
+  public Casa(Coordenada ubicacion, int vida, Player owner, string tipo, int capacidad)
+      : base(ubicacion, vida, owner)
+  {
+      CapacidadPoblacion = capacidad; 
+  }
 }
