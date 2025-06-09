ad2800693a2961e3dfd4f31714f25a7ed15bb576
beluume <maria.mendes@correo.ucu.edu.uy>
2025-06-09 20:19:10 -0300

    arreglos en celda

--- src/Library/Celda.cs
+++ src/Library/Celda.cs
@@ -3,22 +3,15 @@
 public class Celda
 {
     private Coordenada coordenada;
-    private List<ElementoMapa> contenido;
     
     public Celda(Coordenada coordenada)
     {
         this.coordenada = coordenada;
-        this.contenido = new List<ElementoMapa>();
     }
     
     public Coordenada Coordenada
     {
         get { return coordenada; }
         set { coordenada = value; }
-    }
-
-    public List<ElementoMapa> Contenido
-    {
-        get { return contenido; }
     }
 }
