19882a293d8a4c1527c22548726724f3a5aabbb6
beluume <maria.mendes@correo.ucu.edu.uy>
2025-06-18 20:06:46 -0300

    Le puse mayusculas porque me confundi de cuando usaba pascal

--- test/Library.Tests/UnitTest1.cs
+++ test/Library.Tests/UnitTest1.cs
@@ -5,116 +5,117 @@ public class Tests
     [SetUp]
     public void Setup()
     {
+        
     }
 
     //1. poder crear una nueva partida especificando el tamaño del mapa y cantidad de jugadores.
     [Test]
-    public void nuevaPartidaTest()
+    public void NuevaPartidaTest()
     {
         
     }
     
     //2. elegir civilización (aprovechar sus ventajas estratégicas)
     [Test]
-    public void elegirCivilizacion()
+    public void ElegirCivilizacion()
     {
         
     }
     
     //3. comenzar con un centro cívico y algunos aldeanos para iniciar la recolecci{on de recursos.
     [Test]
-    public void edificiosIniciales()
+    public void EdificiosIniciales()
     {
         
     }
     
     //4. ordenar a los aldeanos recolectar diferentes tipos de recursos.
     [Test]
-    public void recoleccionDeDistintosRecursos()
+    public void RecoleccionDeDistintosRecursos()
     {
         
     }
     
     //5. construir edificios para almacenar recursos.
     [Test]
-    public void construccionEdificios()
+    public void ConstruccionEdificios()
     {
         
     }
     
     // 6. quiero visualizar la cantidad de recursos disponibles.
     [Test]
-    public void cantidadRecursos()
+    public void CantidadRecursos()
     {
         
     }
     
     // 7. construir edificios en ubis específicas para expandir la base.
     [Test]
-    public void ubicacionEspecifica()
+    public void UbicacionEspecifica()
     {
         
     }
     
     //8. crear diferentes tipos de edificios con funciones específicas para desarrollar la civilización.
     [Test]
-    public void desarrollarCivilizacionEdificios()
+    public void DesarrollarCivilizacionEdificios()
     {
         
     }
     
     //9. entrrenar unidades militares para defender la base y atacar oponentes.
     [Test]
-    public void entrenarUnidades()
+    public void EntrenarUnidades()
     {
         
     }
     
     //10. mover las unidades por el mapa usando comandos simples.
     [Test]
-    public void moverUnidades()
+    public void MoverUnidades()
     {
         
     }
     
     //11. ordenar a las unidades atacar edeficios o unidades enemigas. 
     [Test]
-    public void atacarEnemigos()
+    public void AtacarEnemigos()
     {
         
     }
     
     //12. entrenar aldeanos parta mejorar la econom{ia y tener suficientes casas para mantener la población.
     [Test]
-    public void entrenarAldeanos()
+    public void EntrenarAldeanos()
     {
         
     }
     
     //13. destruir los centros cívicos para ganar la partida por dominación militar.
     [Test]
-    public void militaresGananPartida()
+    public void MilitaresGananPartida()
     {
         
     }
     
     //14. usar comandos intuitivos para interactuar con el juego 
     [Test]
-    public void interactuarJuego()
+    public void InteractuarJuego()
     {
         
     }
     
     //15. quiero ver un mapa simplificado del juego en ASCII para visualizar la dispo del terreno y unidades. 
     [Test]
-    public void mapaEnASCII()
+    public void MapaEnASCII()
     {
         
     }
 
     //16. como jugador quiero gardar la partida y continuarla más tarde. 
     [Test]
-    public void guardarPartidaTest()
+    public void GuardarPartidaTest()
     {
         
     }
