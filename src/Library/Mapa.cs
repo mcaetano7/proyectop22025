ae209f9bcf9e22fbbd156b51f37d97df49b7c74c
VictoriaMoraless <victoria.moraless@correo.ucu.edu.uy>
2025-06-20 16:23:39 -0300

    mas comentarios

--- src/Library/Mapa.cs
+++ src/Library/Mapa.cs
@@ -2,28 +2,28 @@ namespace Library;
 
 public class Mapa
 {
-    public const int Size = 100;
-    public List<Celda> Celdas { get; private set; } 
+    public const int Size = 100; //tamaño del mapa
+    public List<Celda> Celdas { get; private set; } //lista con las celdas
 
-    public Mapa()
+    public Mapa() //inicializa el mapa con las celdas necesarias
     {
-        Celdas = new List<Celda>();
+        Celdas = new List<Celda>(); //inicializa la lista de celdas
         
-        for (int x = 0; x < Size; x++)
+        for (int x = 0; x < Size; x++) //genera las celdas
         {
             for (int y = 0; y < Size; y++)
             {
-                Coordenada coordenada = new Coordenada(x, y);
-                Celda celda = new Celda(coordenada);
-                Celdas.Add(celda);
+                Coordenada coordenada = new Coordenada(x, y); //coordenada para la ubicacion
+                Celda celda = new Celda(coordenada); //crea la celda en la coordenada
+                Celdas.Add(celda); //agrega la celda en la lista de mapa
             }
         }
     }
     
-    public Celda ObtenerCelda(Coordenada coordenada)
+    public Celda ObtenerCelda(Coordenada coordenada) //obtiene la celda en las coordenadas indicadas
     {
         return Celdas.First(c =>
             c.Coordenada.X == coordenada.X &&
-            c.Coordenada.Y == coordenada.Y);
+            c.Coordenada.Y == coordenada.Y); //busca y retorna la celda que coincida con las coordenadas
     }
 }
