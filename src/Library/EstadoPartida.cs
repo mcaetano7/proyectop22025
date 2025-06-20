abdb3ddeaf0a57abb9ca71b9d847dbe27de97056
VictoriaMoraless <victoria.moraless@correo.ucu.edu.uy>
2025-06-20 15:57:42 -0300

    comentarios

--- src/Library/EstadoPartida.cs
+++ src/Library/EstadoPartida.cs
@@ -2,10 +2,10 @@ namespace Library;
 
 public interface EstadoPartida
 {
-    public string NombrePartida { get; set; }
-    public string NombreJugador { get; set; }
-    public int Level { get; set; }
-    public int Health { get; set; }
-    public List<string> Inventario { get; set; }
+    public string NombrePartida { get; set; } //nombre de la partida
+    public string NombreJugador { get; set; } //nombre del jugador
+    public int Level { get; set; } //nivel en el que se encuentra el jugador
+    public int Health { get; set; } //salud del jugador
+    public List<string> Inventario { get; set; } //lista de objetos que tiene el inventario
     
 }
