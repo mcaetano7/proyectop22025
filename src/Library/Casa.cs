1028ad21cf6672cc3df44ed63aa95b5cb5c39ef2
Mik4desuu <mikaela.maldonado@correo.ucu.edu.uy>
2025-06-22 13:21:52 -0300

    comento aldeano, almacen, arquero, caballeria y casa

--- src/Library/Casa.cs
+++ src/Library/Casa.cs
@@ -2,19 +2,19 @@ namespace Library;
 
 public class Casa : Edificio
 {
-  //capacidad poblacion
+  //capacidad poblacion (cuantos individuos puede alojar esta casa)
   public int CapacidadPoblacion { get; set; }
-  public int CapacidadMaxima = 5;
+  public int CapacidadMaxima = 5; 
   
   
 
-  public Casa(Coordenada ubicacion, int vida, Player owner, string tipo, int capacidad)
+  public Casa(Coordenada ubicacion, int vida, Player owner, string tipo, int capacidad) //crea una casa
       : base(ubicacion, 100, owner)
   {
       Resistencia = 500;
   }
 
-  public override Dictionary<TipoRecurso, int> obtenerCosto()
+  public override Dictionary<TipoRecurso, int> obtenerCosto() //método que devuelve el costo de construir una casa (25 unidades de madera)
   {
       return new Dictionary<TipoRecurso, int>
       {
