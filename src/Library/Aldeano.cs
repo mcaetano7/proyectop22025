da3bcc9a878125583506f3931ef424c26d5c58d7
Mik4desuu <mikaela.maldonado@correo.ucu.edu.uy>
2025-06-22 13:49:20 -0300

    modifico comments de aldeano para que cumpla con doxygen y poder probarlo

--- src/Library/Aldeano.cs
+++ src/Library/Aldeano.cs
@@ -1,23 +1,48 @@
 namespace Library;
-//esta clase representa a los aldeanos
+/// <summary>
+/// esta clase representa a los aldeanos, quienes pueden recolectar recursos
+/// </summary>
 public class Aldeano : Unidad
 {
-    private bool ocupado = false; //indica si el aldeano está ocupado (recolectando recursos) o no
+    /// <summary>
+    /// indica si el aldeano está ocupado (recolectando recursos) o no
+    /// </summary>
+    private bool ocupado = false; 
+    
+    /// <summary>
+    /// construye al aldeano
+    /// </summary>
+    /// <param name="id"> identifica al aldeano </param>
+    /// <param name="ubicacion"> donde se encuentra inicialmente en el mapa</param>
+    /// <param name="owner"> a que jugador le pertenece </param>
     public Aldeano(int id, Coordenada ubicacion, Player owner)
         : base (id, ubicacion, vida: 50, velocidad : 1, owner)
     {}
-
+    /// <summary>
+    /// //m{etodo que cambia la ubi del aldeano por una nueva (mueve al aldeano)
+    /// </summary>
+    /// <param name="nuevaUbicacion"> ubicación a la que el aldeano es movido</param>
     public override void Mover(Coordenada nuevaUbicacion)
     {
-        Ubicacion = nuevaUbicacion;  //m{etodo que cambia la ubi del aldeano por una nueva (mueve al aldeano)
+        Ubicacion = nuevaUbicacion; 
     }
-
+    
+    /// <summary>
+    /// método para saber el estado de un aldeano
+    /// </summary>
+    /// <returns> devuelve true si el aldeano no está ocuado (si no está recolectando recursos) </returns>
     public bool EstaDisponible()
     {
-        return !ocupado; //devuelve true si el aldeano no está ocuado
+        return !ocupado; 
     }
 
-    public void IniciarRecoleccion(TipoRecurso tipo, Coordenada ubicacion) //método para que el aldeano recolecte recursos y los acumule con agregar.recurso
+    
+    /// <summary>
+    /// //método para que el aldeano recolecte recursos y los acumule con agregar.recurso
+    /// </summary>
+    /// <param name="tipo"> indica de que recurso se trata</param>
+    /// <param name="ubicacion"> indica la ubicación de la recolección </param>
+    public void IniciarRecoleccion(TipoRecurso tipo, Coordenada ubicacion) 
     {
         ocupado = true;
         Owner.AgregarRecurso(tipo, 10);
--- src/Library/Arquero.cs
+++ src/Library/Arquero.cs
@@ -1,5 +1,10 @@
 namespace Library;
-//representa los arqueros (una unidad militar)
+/// <summary>
+/// representa a los arqueros (una unidad militar)
+/// </summary>
+/// <param name="id"> identifica al arquero </param>
+/// <param name="ubicacion"> donde se encuentra en el mapa </param>
+/// <param name="owner"> jugador al que pertenece el arquero </param>
 public class Arquero : UnidadMilitar
 {
     public Arquero(int id, Coordenada ubicacion, Player owner) 
