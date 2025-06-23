d810e5737aeec38d388a19299bf8d8d6c58b3672
VictoriaMoraless <victoria.moraless@correo.ucu.edu.uy>
2025-06-23 11:19:26 -0300

    + com

--- src/Library/Infanteria.cs
+++ src/Library/Infanteria.cs
@@ -1,9 +1,18 @@
 namespace Library;
 
+/// <summary>
+/// Clase que representa una unidad de infantería militar
+/// </summary>
 public class Infanteria : UnidadMilitar
 {
-    public Infanteria(int id, Coordenada ubicacion, Player owner) //inicializa una unidad infanteria ya definida
-        : base(id, ubicacion, vida: 100, velocidad: 50, owner, ataque: 30, defensa: 50) //ubicacion, puntos de vida, velocidad, propietario, ataque, defensa
+    /// <summary>
+    /// Constructor que inicializa una nueva infantería
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="ubicacion"></param>
+    /// <param name="owner"></param>
+    public Infanteria(int id, Coordenada ubicacion, Player owner) 
+        : base(id, ubicacion, vida: 100, velocidad: 50, owner, ataque: 30, defensa: 50) 
     {
     } 
 }
