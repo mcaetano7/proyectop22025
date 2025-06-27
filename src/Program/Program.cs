431f2963fa0b80b79b2a0361cb90c123be8fc965
mcaetano7 <martina.caetano@correo.ucu.edu.uy>
2025-06-26 21:50:59 -0300

    bot

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,35 +1,35 @@
-﻿namespace Program
+﻿using Ucu.Poo.DiscordBot.Domain;
+using Ucu.Poo.DiscordBot.Services;
+
+namespace Program;
+
+/// <summary>
+/// Un programa que implementa un bot de Discord.
+/// </summary>
+internal static class Program
 {
-    using Library;
-
     /// <summary>
-    /// clase principal que arranca la aplicación
+    /// Punto de entrada al programa.
     /// </summary>
-    class Program
+   
+    private static void Main()
     {
-        /// <summary>
-        /// método main que crea la partida y los jugadores
-        /// </summary>
-        /// <param name="args">argumentos de línea de comando</param>
-        static void Main(string[] args)
-        {
-            // crear civilizaciones
-            Civilizacion civ1 = new Civilizacion("bizantinos", new List<string>());
-            Civilizacion civ2 = new Civilizacion("japanese", new List<string>());
-
-            // crear jugadores con civilizaciones
-            Player jugador1 = new Player("Mika", civ1);
-            Player jugador2 = new Player("Victoria", civ2);
-
-            // crear fachada del juego
-            Facade juego = new Facade();
-
-            // crear partida con las civilizaciones
-            juego.CrearPartida(civ1, civ2);
-
-            // inicializar juego para ambos jugadores
-            juego.Jugador1.InicializarJuego();
-            juego.Jugador2.InicializarJuego();
-        }
+        //DemoFacade();
+        DemoBot();
     }
+    
+    private static void DemoFacade()
+    {
+        Console.WriteLine(Facade.Instance.AddTrainerToWaitingList("player"));
+        Console.WriteLine(Facade.Instance.AddTrainerToWaitingList("opponent"));
+        Console.WriteLine(Facade.Instance.GetAllTrainersWaiting());
+        Console.WriteLine(Facade.Instance.StartBattle("player", "opponent"));
+        Console.WriteLine(Facade.Instance.GetAllTrainersWaiting());
+    }
+
+    private static void DemoBot()
+    {
+        BotLoader.LoadAsync().GetAwaiter().GetResult();
+    }
+    
 }
